ecd8637c163b8ba873fabce4f131dda12582b4bc
Mazlum200 <daskin_daskin@hotmail.com>
2018-01-22 23:44:25 +0300

    create controller

---
+++ src/com/atm/Account.java
@@ -0,0 +1,18 @@
+package com.atm;
+import java.util.ArrayList;
+
+public class Account {
+	
+	private String name;
+	
+	private double balance;
+	
+	private String uuid;
+	
+	private User holder;
+	
+	private ArrayList<Transaction> transactions;
+	
+	
+
+}
